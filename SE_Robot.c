3dc12a78904f228a58da6b41b6aa716d76ce0614
garyo <g15218@g.ichinoseki.ac.jp>
2019-01-30 14:37:00 +0900

    2019/01/30_授業分

--- SE_Robot.c
+++ SE_Robot.c
@@ -13,23 +13,30 @@
 #define KI 0.0
 #define KD 0.0
 #define M_PI 3.1415926535
-#define K_ANGLE_CORRECT 0.93994778067
-#define LINE_SENSOR_TH 600
+#define K_ANGLE_CORRECT 1 //0.93994778067
+#define LINE_SENSOR_TH 500
 const char target_subsystem_name[] = "OSEK SE_Robot";
 
 // フラグ変数
 U8 clip_num = 0;
 U8 is_line = 1;
 U8 is_discovered = 0;
+U8 sound_flag = 0;
+U8 sound_clip = 0;
+U8 flag_num = 0;
 
 // データ変数
+U16 ls_val;
 F32 mt_n_L = 0;
 F32 mt_n_R = 0;
 F32 robot_angle = 0;
 
+
+
 // 関数宣言
 void lineTrace(U8 is_line, U8 line_side, U32 duty_h, U32 duty_l);
 void appeal();
+F32 calPID(U16 sensor_val, U16 target_val, F32 dt);
 
 void ecrobot_device_initialize(void)
 {
@@ -58,16 +65,18 @@ void user_1ms_isr_type2(void)
 
 TASK(DisplayTask)
 {
-    ecrobot_status_monitor(target_subsystem_name);
+    // ecrobot_status_monitor(target_subsystem_name);
 
-    // display_goto_xy(0, 0);
-    // display_int(is_line, 15);
-    // display_goto_xy(0, 1);
-    // display_int((int)mt_n_L, 15);
-    // display_goto_xy(0, 2);
-    // display_int((int)mt_n_R, 15);
-    // display_goto_xy(0, 3);
-    // display_int((int)robot_angle, 15);
+    display_goto_xy(0, 0);
+    display_int(ls_val, 15);
+    display_goto_xy(0, 1);
+    display_int(is_line, 15);
+    display_goto_xy(0, 2);
+    display_int((int)mt_n_L, 15);
+    display_goto_xy(0, 3);
+    display_int((int)mt_n_R, 15);
+    display_goto_xy(0, 4);
+    display_int((int)robot_angle, 15);
     display_update();
 
     TerminateTask();
@@ -81,11 +90,11 @@ TASK(TouchSensorTask)
 TASK(ActionTask)
 {
     static F32 dt = 0.004;
-    U32 duty_h = 70;
-    U32 duty_l = 50;
+    U32 duty_h = 85;
+    U32 duty_l = 25;
 
     // センサの値処理（２値化）
-    U16 ls_val = ecrobot_get_light_sensor(PORT_LINE_SENS);
+    ls_val = ecrobot_get_light_sensor(PORT_LINE_SENS);
     is_line = (ls_val > LINE_SENSOR_TH) ? 1 : 0;
 
     // 車輪のオドメトリ計算[回転]
@@ -93,7 +102,7 @@ TASK(ActionTask)
     mt_n_R = nxt_motor_get_count(PORT_R_MT) / 360.;
 
     // 自己角度推定
-    robot_angle = K_ANGLE_CORRECT * 180 * 40.8 / 52 * (mt_n_R - mt_n_L);
+    robot_angle = K_ANGLE_CORRECT * 180 * (55/2) / 52 * (mt_n_R - mt_n_L);
 
     // 移動のシーケンス
     switch (clip_num)
@@ -108,8 +117,7 @@ TASK(ActionTask)
     // ライントレース開始
     // 次シーケンス遷移トリガ: 障害物検出
     case 1:
-        is_discovered = 1;
-        lineTrace(is_line, 1, duty_h, duty_l);
+        lineTrace(is_line, 0, duty_h, duty_l);
 
         if (ecrobot_get_touch_sensor(PORT_SENSE_BIT))
             clip_num++;
@@ -118,11 +126,12 @@ TASK(ActionTask)
     // 旋回
     // 次シーケンス遷移トリガ: 角度収束
     case 2:
-        nxt_motor_set_speed(PORT_R_MT, -70, 0);
-        nxt_motor_set_speed(PORT_L_MT, 70, 0);
+        is_discovered = 1;
+        nxt_motor_set_speed(PORT_R_MT, 70, 0);
+        nxt_motor_set_speed(PORT_L_MT, -70, 0);
         // robot_angle = K_ANGLE_CORRECT * 180 * 40.8 / 52 * (mt_n_R - mt_n_L);
 
-        if (robot_angle < -450)
+        if (robot_angle > -90)
             clip_num++;
         break;
 
@@ -155,6 +164,7 @@ void lineTrace(U8 is_line, U8 line_side, U32 duty_h, U32 duty_l)
         {
             nxt_motor_set_speed(PORT_R_MT, duty_l, 0);
             nxt_motor_set_speed(PORT_L_MT, duty_h, 0);
+
         }
         else
         {
@@ -177,8 +187,129 @@ void lineTrace(U8 is_line, U8 line_side, U32 duty_h, U32 duty_l)
     }
 }
 
+int saund_num = 0;
+int sound(U32 freq, U32 ms, U32 vol)
+{
+    int do_next = 0;
+    ecrobot_sound_tone(freq, 4, vol);
+
+    saund_num++;
+
+    if(saund_num*4 > ms)
+    {
+        saund_num = 0;
+        do_next = 1;
+    }
+
+    return do_next;
+}
+
+
 void appeal()
 {
+  if(sound_flag == 0)
+  {
+    switch (sound_clip)
+    {
+      case 0:
+          sound_clip += sound(196, 170, 100);    // ソ
+          break;
+      case 1:
+          sound_clip += sound(262, 170, 100);    // ド
+          break;
+      case 2:
+          sound_clip += sound(330, 170, 100);    // ミ
+          break;
+      case 3:
+          sound_clip += sound(392, 170, 100);    // ソ
+          break;
+      case 4:
+          sound_clip += sound(523, 170, 100);    // ド
+          break;
+      case 5:
+          sound_clip += sound(659, 170, 100);    // ミ
+          break;
+      case 6:
+          sound_clip += sound(784, 510, 100);    // ソ
+          break;
+      case 7:
+          sound_clip += sound(659, 510, 100);    // ミ
+          break;
+      case 8:
+          sound_clip += sound(208, 170, 100);    // ラ♭
+          break;
+      case 9:
+          sound_clip += sound(262, 170, 100);    // ド
+          break;
+      case 10:
+          sound_clip += sound(311, 170, 100);    // ミ♭
+          break;
+      case 11:
+          sound_clip += sound(415, 170, 100);    // ラ♭
+          break;
+      case 12:
+          sound_clip += sound(523, 170, 100);    // ド
+          break;
+      case 13:
+          sound_clip += sound(622, 170, 100);    // ミ♭♭
+          break;
+      case 14:
+          sound_clip += sound(831, 510, 100);    // ラ♭
+          break;
+      case 15:
+          sound_clip += sound(622, 510, 100);    // ミ
+          break;
+      case 16:
+          sound_clip += sound(233, 170, 100);    // シ
+          break;
+      case 17:
+          sound_clip += sound(294, 170, 100);    // レ
+          break;
+      case 18:
+          sound_clip += sound(349, 170, 100);    // ファ
+          break;
+      case 19:
+          sound_clip += sound(466, 170, 100);    // シ♭
+          break;
+      case 20:
+          sound_clip += sound(587, 170, 100);    // レ
+          break;
+      case 21:
+          sound_clip += sound(698, 170, 100);    // ファ
+          break;
+      case 22:
+          sound_clip += sound(932, 510-85, 100);    // シ♭
+          break;
+      case 23:
+          sound_clip += sound(932, 85, 0);    // 休符
+          break;
+      case 24:
+          sound_clip += sound(932, 170, 100);    // シ♭
+          break;
+      case 25:
+          sound_clip += sound(932, 85, 0);    // 休符
+          break;
+      case 26:
+          sound_clip += sound(932, 85, 100);    // シ♭
+          break;
+      case 27:
+          sound_clip += sound(932, 85, 0);    // 休符
+          break;
+      case 28:
+          sound_clip += sound(932, 85, 100);    // シ♭
+          break;
+      case 29:
+          sound_clip += sound(932, 85, 0);    // 休符
+          break;
+      case 30:
+          sound_clip += sound(1047,2040, 100);  // ド
+          break;
+      default:
+          sound_flag = 1;
+          break;
+    }
+  }
+
     if (nxt_motor_get_count(PORT_FLAG_MT) < 620)
         nxt_motor_set_speed(PORT_FLAG_MT, 80, 0);
     else
@@ -187,13 +318,13 @@ void appeal()
 
 static S32 deff[2];
 static F32 integral;
-F32 calPID(U16 sensor_val, U16 target_val)
+F32 calPID(U16 sensor_val, U16 target_val, F32 dt)
 {
     F32 p, i, d;
 
     deff[0] = deff[1];
     deff[1] = sensor_val - target_val;
-    indtegral += (deff[0] + deff[1]) / dt;
+    integral += (deff[0] + deff[1]) / dt;
 
     p = KP * deff[1];
     i = KI * integral;
